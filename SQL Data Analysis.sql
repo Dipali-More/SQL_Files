cff10d853c98d3e7daf723db03af99dade6930dc
Dmore <49077847+Dipali-More@users.noreply.github.com>
2023-09-16 20:17:46 -0400

    Update SQL Data Analysis.sql

--- SQL Data Analysis.sql
+++ SQL Data Analysis.sql
@@ -8,28 +8,11 @@ select * from passenger_count;
 select * from datetime_dim;
 select * from pickup;
 select * from dropoff;
-
-
-
 --Find the total_fare by each vendor
 
 select  VendorID, sum(fare_amount) as total_fare from uber group by VendorID;
-
-
-
 select b.payment_type_name, avg(tip_amount) as Total_tip from uber a
 join payment_type b on a.payment_type = b.payment_type group by payment_type_name
 ORDER BY Total_tip DESC;
---FIND TOTAL NUMBER OF TRIPS BY THE PASSENGER COUNTS
-
-
-
---FIND THE TOP 10 PICKUP LOCATIONS BASED ON THE NUMBER OF TRIPS
-
-
-
---FIND THE AVERAGE FARE AMOUNT BY HOUR OF THE DAY
-
---
 
 
